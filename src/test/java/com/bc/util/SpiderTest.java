d2e216e61fdc5ac496eda21fb341dce94cca347e
gaoyun <625605841@qq.com>
2018-03-16 08:40:26 +0800

    update 1

---
+++ src/test/java/com/bc/util/SpiderTest.java
@@ -0,0 +1,41 @@
+package com.bc.util;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.alibaba.fastjson.JSONObject;
+import com.bc.vo.Message;
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
+public class SpiderTest {
+
+	@Test
+	public void testGet() {
+		String url = "http://localhost:8080/bc/init";
+		String response = Spider.get(url);
+		System.out.println(response);
+		Assert.assertNotNull(response);
+	}
+
+	@Test
+	public void testPost() {
+		String url = "http://localhost:8080/bc/add";
+		Message m = new Message();
+		m.setVac(123);
+		String body = JSONObject.toJSONString(m);
+		String response = Spider.post(url, body);
+		System.out.println(response);
+		Assert.assertNotNull(response);
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
